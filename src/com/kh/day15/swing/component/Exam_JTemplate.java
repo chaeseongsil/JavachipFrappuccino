083b3f2a2ec4b48c4f20c262428b1fb84fd563c1
chaeseongsil <cotud91822@gmail.com>
2023-07-14 17:49:53 +0900

    [ADD] 23/07/14 고급 스윙 컴포넌트
    1. JMenuBar 컴포넌트 만들기 예제
    2. JToolBar 컴포넌트 만들기 예제
    3. JDialog 컴포넌트 만들기 예제
    4. JTabbedPane 컴포넌트 만들기 예제

---
+++ src/com/kh/day15/swing/component/Exam_JTemplate.java
@@ -0,0 +1,23 @@
+package com.kh.day15.swing.component;
+
+import java.awt.Container;
+
+import javax.swing.JFrame;
+
+public class Exam_JTemplate extends JFrame{
+	
+	public Exam_JTemplate() {
+		setTitle("메뉴 만들기 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container c = getContentPane();
+		
+		setSize(250, 200);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Exam_JTemplate();
+	}
+
+}
